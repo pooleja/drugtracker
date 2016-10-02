c17ec03e88063d07e2b6512303ec668731e3e054
austinban <banaustinban@gmail.com>
2016-10-01 23:35:56 -0500

    fixed commit issue

--- config/env.js
+++ config/env.js
@@ -1,6 +1,3 @@
 exports.COOKIE_SECRET = process.env.COOKIE_SECRET;
-<<<<<<< HEAD
-=======
 exports.USER = process.env.USER;
-exports.PASS = process.env.PASS:
->>>>>>> 6084ed1367034d3669864c1fb36287b843c5bef6
+exports.PASS = process.env.PASS;
